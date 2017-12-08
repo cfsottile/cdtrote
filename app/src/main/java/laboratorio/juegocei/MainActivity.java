e4ac1866c67af79930bf34cf2a30f6493f41f750
Cristian Sottile <cfsottile@gmail.com>
2017-12-08 18:45:09 -0300

    Resolved #7

--- app/src/main/java/laboratorio/juegocei/MainActivity.java
+++ app/src/main/java/laboratorio/juegocei/MainActivity.java
@@ -1,33 +1,43 @@
 package laboratorio.juegocei;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Display;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        Bitmap bmp = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+            getResources(),R.drawable.portada_xl),size.x,size.y,true);
 
-        Button playButton = (Button) findViewById(R.id.play);
+        ImageView iv_background = findViewById(R.id.background);
+        iv_background.setImageBitmap(bmp);
+
+        Button playButton = findViewById(R.id.play);
+        playButton.setX(size.x / 2 - 125);
+        playButton.setY(size.y / 2);
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
